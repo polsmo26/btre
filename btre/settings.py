56dc71d2bf160ecc087526f7de13dbcda5a21482
Polina <polsmo26@bergen.org>
2025-03-04 10:13:57 -0500

    3/4

--- btre/settings.py
+++ btre/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhitenoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'btre.urls'
